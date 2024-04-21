fe2c4043b4ceb2ba5a103cd4e37f921249be8f87
Ramanjaneya <RamanjaneyaDarna.NarasimhaRao@arup.com>
2024-04-21 23:23:36 +0530

    Updated till block section

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,6 +1,15 @@
 from textnode import (TextNode, text_type_text, text_type_bold, text_type_italic, text_type_code, text_type_image, text_type_link)
 import re
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, '**', text_type_bold)
+    nodes = split_nodes_delimiter(nodes, '*', text_type_italic)
+    nodes = split_nodes_delimiter(nodes, '`', text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
